857c346ded6899bd9f116618b29b044041162ebf
transformers <raminq@hotmail.com>
2023-02-21 19:55:46 -0500

    updated code

--- src/test/GitPractaceTwo.java
+++ src/test/GitPractaceTwo.java
@@ -3,8 +3,7 @@ package test;
 public class GitPractaceTwo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("this is for push practace");
 	}
 
 }
